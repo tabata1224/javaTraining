5b9ce7ce59c14b24912df4a38e90cb2593b10d52
Kitano <ryohei.kitano.works@gmail.com>
2022-12-13 04:37:05 +0000

    feat: チャプター1の問題を追加

--- JavaTraining/java-practice/src/main/java/consallink/chapter1/Training1.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter1/Training1.java
@@ -17,4 +17,123 @@ public class Training1 {
         return answer;
     }
 
+    public int practice2() {
+
+        // 問題2
+        // JREの説明として間違っているものを選んでください
+        // 1.JREはJavaの実行環境で、Javaプログラムを実行するにはJREが必要である
+        // 2.JDKとJREは全く別のものの為、JDkインストール後にJREを必ずインストールする必要がある
+        // 3.JREのみではJavaの開発を行うことはできない為、JDKのインストールが必要である
+        // 4.JREは「Java Runtime Environment」を略したものです
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice3() {
+
+        // 問題3
+        // JVMの説明として間違っているものを選んでください
+        // 1.JVMは「Java Virtual Machine」を略したものです
+        // 2.JREの一部としてJVMもインストールされる
+        // 3.JVMがコンパイル時にエラーを表示してくれる
+        // 4.JVMがJavaバイトコードをその環境に応じた実行形式のコードに変換してくれる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice4() {
+
+        // 問題4
+        // コンパイルの説明として間違っているものを選んでください
+        // 1.コンパイル時には「javac ファイル名」でコンパイルを行う
+        // 2.コンパイル時には「java ファイル名」でコンパイルを行う
+        // 3.Javaではコンパイルを行うことでクラスファイルが作成される
+        // 4.コンパイル時のファイル名は拡張子も記述する必要がある
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice5() {
+
+        // 問題5
+        // Test.classというファイルを実行する時の説明として間違っているものを選んでください
+        // 1.「java クラス名」で実行する
+        // 2.「javac クラス名」で実行する
+        // 3.クラスファイルが作成されていないと実行はできない
+        // 4.実行時のクラス名は拡張子は記述しない
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice6() {
+
+        // 問題6
+        // Javaをコーディングする際のルールとして間違っているものを選んでください
+        // 1.大文字と小文字は区別される
+        // 2.スペースに全角スペースを用いるとエラーとなる
+        // 3.スペースを利用する際、2文字以上のスペースを入力するとエラーとなる
+        // 4.classやvoidなどは全角で入力するとエラーとなる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice7() {
+
+        // 問題7
+        // 出力処理の記述として正しいものを選んでください
+        // 1.system.out.println(“表示”);
+        // 2.System.out.println(“表示”);
+        // 3.system,out,println(“表示”);
+        // 4.System,out,println(“表示”);
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice8() {
+
+        // 問題8
+        // Javaプログラムを実行するまでの手順として適切なものを選んでください
+        // 1.ソースコード入力->コンパイル->実行
+        // 2.ソースコード入力->実行->コンパイル
+        // 3.コンパイル->ソースコード入力->実行
+        // 4.ソースコード入力->実行
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice9() {
+
+        // 問題9
+        // Javaプログラム実行する為のmain()メソッドとして適切なものを選んでください
+        // 1.public static void main(string[] args){処理;}
+        // 2.public void main(String[] args){処理;}
+        // 3.public static void Main(String[] args){処理;}
+        // 4.public static void main(String[] args){処理;}
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
 }
--- JavaTraining/java-practice/src/test/java/consallink/chapter1/Training1_Test.java
+++ JavaTraining/java-practice/src/test/java/consallink/chapter1/Training1_Test.java
@@ -5,11 +5,68 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Training1_Test {
+    private Training1 training1 = new Training1();
+
     @Test
-    public void Practice1() throws Exception {
-        Training1 training1_1 = new Training1();
+    public void practice1() throws Exception {
         int expected = 1;
-        int actual = training1_1.practice1();
+        int actual = training1.practice1();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice2() throws Exception {
+        int expected = 2;
+        int actual = training1.practice2();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice3() throws Exception {
+        int expected = 3;
+        int actual = training1.practice3();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice4() throws Exception {
+        int expected = 2;
+        int actual = training1.practice4();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice5() throws Exception {
+        int expected = 2;
+        int actual = training1.practice5();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice6() throws Exception {
+        int expected = 3;
+        int actual = training1.practice6();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice7() throws Exception {
+        int expected = 2;
+        int actual = training1.practice7();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice8() throws Exception {
+        int expected = 1;
+        int actual = training1.practice8();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice9() throws Exception {
+        int expected = 4;
+        int actual = training1.practice9();
         assertThat(actual, is(expected));
     }
 }
