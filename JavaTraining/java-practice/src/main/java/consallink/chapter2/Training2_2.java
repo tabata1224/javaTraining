8d98d02f12515eb7e4e15a756e40a9d36e0155b4
Kitano <ryohei.kitano.works@gmail.com>
2022-12-13 06:04:14 +0000

    feat: チャプター2_2の問題を追加

--- JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_2.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_2.java
@@ -16,4 +16,146 @@ public class Training2_2 {
 
         return answer;
     }
+
+    public int practice2() {
+
+        // 問題2
+        // String型の説明として間違っているものを選んでください
+        // 1.String型は比較演算子「==」を利用して同じ文字列かどうかを比較できる
+        // 2.「String型の変数.equals(String型) 」を利用して文字列を比較できる
+        // 3.String型+String型は加算ではなく文字列連結となる
+        // 4.+の左辺か右辺いずれかにString型が含まれている場合は文字列連結となる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice3() {
+
+        // 問題3
+        // 比較演算の説明として間違っているものを選んでください
+        // 1.double型とint型の比較演算は型が違うためできない
+        // 2.String型同士で「==」を行うとコンパイルエラーにはならない。
+        // 3.比較演算と算術演算では算術演算の方が先に行われる
+        // 4.char型は内部で数値を扱っているため、比較演算で比較が可能
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice4() {
+
+        // 問題4
+        // 論理演算の説明として間違っているものを選んでください
+        // 1.論理演算の被演算子はboolean型を利用する
+        // 2.論理演算の演算結果はboolean型となる
+        // 3.論理演算の&&は左辺と右辺がどちらもtrueの時のみtrueの結果となる
+        // 4.論理演算の||は左辺と右辺がどちらもtrueの時のみtrueの結果となる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice5() {
+
+        // 問題5
+        // コンパイルエラーとなる記述を選んでください
+        // 1.true && true || false
+        // 2.35 > 10 > 25
+        // 3.35 > 10 && 10 > 25
+        // 4.35 > 10 && true
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice6() {
+
+        // 問題6
+        // String型のxとString型のyが初期化されて定義されているとき、コンパイルエラーとなる記述を選んでください
+        // 1.boolean z = x.equals(y);
+        // 2.boolean z = x == y;
+        // 3.boolean z = x >= y;
+        // 4.boolean z = x != y;
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice7() {
+
+        // 問題7
+        // System.out.println(“1000” + 20);
+        // 上記の実行結果として正しいものを選んでください
+        // 1.100020
+        // 2.1020
+        // 3.コンパイルエラー
+        // 4.10020
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice8() {
+
+        // 問題8
+        // System.out.println(“1000” * 20);
+        // 上記の実行結果として正しいものを選んでください
+        // 1.100020
+        // 2.20000
+        // 3.コンパイルエラー
+        // 4.2000
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice9() {
+
+        // 問題9
+        // System.out.println(50 > 30);
+        // 上記の実行結果として正しいものを選んでください
+        // 1.コンパイルエラー
+        // 2.true
+        // 3.false
+        // 4.50
+        // 5.30
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice10() {
+
+        // 問題10
+        // boolean b = false;
+        // System.out.println(b = 50 < 30);
+        // 上記の実行結果として正しいものを選んでください
+        // 1.コンパイルエラー
+        // 2.true
+        // 3.false
+        // 4.50
+        // 5.30
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
 }
--- JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_2_Test.java
+++ JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_2_Test.java
@@ -5,11 +5,75 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Training2_2_Test {
+    private Training2_2 training2_2 = new Training2_2();
+
     @Test
-    public void Practice1() throws Exception {
-        Training2_2 training2_2 = new Training2_2();
+    public void practice1() throws Exception {
         int expected = 2;
         int actual = training2_2.practice1();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice2() throws Exception {
+        int expected = 1;
+        int actual = training2_2.practice2();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice3() throws Exception {
+        int expected = 1;
+        int actual = training2_2.practice3();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice4() throws Exception {
+        int expected = 4;
+        int actual = training2_2.practice4();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice5() throws Exception {
+        int expected = 2;
+        int actual = training2_2.practice5();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice6() throws Exception {
+        int expected = 3;
+        int actual = training2_2.practice6();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice7() throws Exception {
+        int expected = 1;
+        int actual = training2_2.practice7();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice8() throws Exception {
+        int expected = 3;
+        int actual = training2_2.practice8();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice9() throws Exception {
+        int expected = 2;
+        int actual = training2_2.practice9();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice10() throws Exception {
+        int expected = 3;
+        int actual = training2_2.practice10();
         assertThat(actual, is(expected));
     }
 }
