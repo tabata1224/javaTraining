551ec84bec4f9f963663465035bb3d1e121bc650
Kitano <ryohei.kitano.works@gmail.com>
2022-12-13 08:11:58 +0000

    feat: チャプター2_3の問題を追加

--- JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_3_Test.java
+++ JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_3_Test.java
@@ -10,9 +10,10 @@ import java.util.List;
 import org.junit.Test;
 
 public class Training2_3_Test {
+    private Training2_3 training2_3 = new Training2_3();
 
     @Test
-    public void Practice1() throws Exception {
+    public void practice1() throws Exception {
 
         final List<String> actual = new ArrayList<String>();
         PrintStream fakePrintStream = new PrintStream("aaa") {
@@ -24,11 +25,30 @@ public class Training2_3_Test {
 
         // Act
         System.setOut(fakePrintStream);
-        Training2_3 training2_3 = new Training2_3();
         training2_3.practice1();
 
         // Assert
         List<String> expected = Arrays.asList("プログラミング言語の種類", "「Java」「C言語」「PHP」などがあります。");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void practice2() throws Exception {
+
+        final List<String> actual = new ArrayList<String>();
+        PrintStream fakePrintStream = new PrintStream("aaa") {
+            @Override
+            public void println(String str) {
+                actual.add(str);
+            }
+        };
+
+        // Act
+        System.setOut(fakePrintStream);
+        training2_3.practice2();
+
+        // Assert
+        List<String> expected = Arrays.asList("研修の講師はとても優しくて、", "真面目ないい人です。");
         assertEquals(actual, expected);
     }
 }
