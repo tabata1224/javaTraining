c6e71189c83cfd7bc584ba60fbe720cde1ee8bfe
Kitano <ryohei.kitano.works@gmail.com>
2022-12-13 05:00:31 +0000

    feat: チャプター2の問題を追加

--- JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_1.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_1.java
@@ -16,4 +16,198 @@ public class Training2_1 {
 
         return answer;
     }
+
+    public int practice2() {
+
+        // 問題2
+        // 整数型の変数として間違っているものを選んでください
+        // 1.byte
+        // 2.short
+        // 3.int
+        // 4.float
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice3() {
+
+        // 問題3
+        // double型の変数に代入できない値を選んでください
+        // 1.2.5F
+        // 2.2.5D
+        // 3.‘a’
+        // 4.“a”
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice4() {
+
+        // 問題4
+        // char型の変数に代入できない値を選んでください
+        // 1.“a”
+        // 2.‘a’
+        // 3.‘あ’
+        // 4.128
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice5() {
+
+        // 問題5
+        // int型の変数に代入するとエラーになる記述を選んでください
+        // 1.5 / 2
+        // 2.4 / 2
+        // 3.4.0 / 2
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice6() {
+
+        // 問題6
+        // 2進数の値を扱うための記述として正しいものを選んでください
+        // 1.0b111
+        // 2.0x111
+        // 3.0111
+        // 4.b111
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice7() {
+
+        // 問題7
+        // 16進数の値を扱うための記述として正しいものを選んでください
+        // 1.0b111
+        // 2.0x111
+        // 3.0111
+        // 4.x111
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice8() {
+
+        // 問題8
+        // キャストの正しい記述方法を選んでください
+        // 1.[double]5
+        // 2.<double>5
+        // 3.(double)5
+        // 4.{double}5
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice9() {
+
+        // 問題9
+        // int a = 6 / (3 / 2) ; の記述においてaの変数に代入される結果を選んでください
+        // 1.3
+        // 2.4
+        // 3.5
+        // 4.6
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice10() {
+
+        // 問題10
+        // プリミティブ型ではない型を選んでください
+        // 1.int
+        // 2.String
+        // 3.double
+        // 4.short
+        // 5.boolean
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice11() {
+
+        // 問題11
+        // 演算結果が2.5になるものを選んでください
+        // 1.(double)(5 / 2)
+        // 2.5 / 2
+        // 3.(double)5 / 2
+        // 4.(int)5 / 2
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice12() {
+
+        // 問題12
+        // 代入演算の説明として間違っているものを選んでください
+        // 1.代入演算は自動的に型変換が行われる
+        // 2.代入時には大きい型から小さい型に型変換される
+        // 3.代入時には小さい型から大きい型に型変換される
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice13() {
+
+        // 問題13
+        // 算術演算の説明として間違っているものを選んでください
+        // 1.算術演算は自動的に型変換が行われ、同じ型同士の演算を行う
+        // 2.算術演算の結果は被演算子の型と同一となる。例えば、int同士の演算では結果もintとなる
+        // 3.boolean型は算術演算が可能
+        // 4.char型は算術演算が可能
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice14() {
+
+        // 問題14
+        // キャストの説明として間違っているものを選んでください
+        // 1.キャストではどんな型でも必ず強制的に型変換ができる
+        // 2.(型) と記述することで指定した型に型変換ができる
+        // 3.double型からint型へキャストで型変換できる
+        // 4.int型からchar型へキャストで型変換できる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
 }
--- JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_1_Test.java
+++ JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_1_Test.java
@@ -5,11 +5,103 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class Training2_1_Test {
+    private Training2_1 training2_1 = new Training2_1();
+
     @Test
-    public void Practice1() throws Exception {
-        Training2_1 training2_1 = new Training2_1();
+    public void practice1() throws Exception {
         int expected = 4;
         int actual = training2_1.practice1();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice2() throws Exception {
+        int expected = 4;
+        int actual = training2_1.practice2();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice3() throws Exception {
+        int expected = 4;
+        int actual = training2_1.practice3();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice4() throws Exception {
+        int expected = 1;
+        int actual = training2_1.practice4();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice5() throws Exception {
+        int expected = 3;
+        int actual = training2_1.practice5();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice6() throws Exception {
+        int expected = 1;
+        int actual = training2_1.practice6();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice7() throws Exception {
+        int expected = 2;
+        int actual = training2_1.practice7();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice8() throws Exception {
+        int expected = 3;
+        int actual = training2_1.practice8();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice9() throws Exception {
+        int expected = 4;
+        int actual = training2_1.practice9();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice10() throws Exception {
+        int expected = 2;
+        int actual = training2_1.practice10();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice11() throws Exception {
+        int expected = 3;
+        int actual = training2_1.practice11();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice12() throws Exception {
+        int expected = 2;
+        int actual = training2_1.practice12();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice13() throws Exception {
+        int expected = 3;
+        int actual = training2_1.practice13();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice14() throws Exception {
+        int expected = 1;
+        int actual = training2_1.practice14();
         assertThat(actual, is(expected));
     }
 }
