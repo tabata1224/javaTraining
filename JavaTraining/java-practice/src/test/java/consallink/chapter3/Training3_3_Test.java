b031058e13de15666ea8481517994b037c867e66
Kitano <ryohei.kitano.works@gmail.com>
2022-12-14 08:25:48 +0000

    feat: チャプター3_3の問題を追加

---
+++ JavaTraining/java-practice/src/test/java/consallink/chapter3/Training3_3_Test.java
@@ -0,0 +1,26 @@
+package consallink.chapter3;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import consallink.StandardOutputSnatcher;
+
+public class Training3_3_Test {
+    Training3_3 training3_3 = new Training3_3();
+    private StandardOutputSnatcher sos;
+
+    public Training3_3_Test() {
+        sos = StandardOutputSnatcher.getInstance();
+        sos.snatch();
+    }
+
+    @Test
+    public void practice1() throws Exception {
+
+        training3_3.practice1();
+        sos.clearOutput();
+        String actual = sos.getOutput();
+        String expected = "田中さんの国語の点数は85点です。\n田中さんの数学の点数は66点です。\n田中さんの平均点は75.5点です。\n合格です。\n";
+
+        assertEquals(actual, expected);
+    }
+}
