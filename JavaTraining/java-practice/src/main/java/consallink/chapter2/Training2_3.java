078313190f8a025b61fc9ce63323259769d184fe
Kitano <ryohei.kitano.works@gmail.com>
2022-12-14 03:02:37 +0000

    refactor: コンソール出力テストを終生

--- JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_3.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_3.java
@@ -3,8 +3,6 @@ package consallink.chapter2;
 public class Training2_3 {
 
     public void practice1() {
-
-        // 問題1
         // 以下の文字列をそのまま出力するプログラムを記述してください。
         // プログラミング言語の種類
         // 「Java」「C言語」「PHP」などがあります。
@@ -12,8 +10,6 @@ public class Training2_3 {
     }
 
     public void practice2() {
-
-        // 問題2
         // 以下の文字列をそのまま出力するプログラムを記述してください。
         // 研修の講師はとても優しくて、
         // 真面目ないい人です。
--- JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_3_Test.java
+++ JavaTraining/java-practice/src/test/java/consallink/chapter2/Training2_3_Test.java
@@ -1,54 +1,37 @@
 package consallink.chapter2;
 
 import static org.junit.Assert.*;
-
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Test;
+import consallink.StandardOutputSnatcher;
 
 public class Training2_3_Test {
     private Training2_3 training2_3 = new Training2_3();
+    private StandardOutputSnatcher sos;
+
+    public Training2_3_Test() {
+        sos = StandardOutputSnatcher.getInstance();
+        sos.snatch();
+    }
 
     @Test
     public void practice1() throws Exception {
 
-        final List<String> actual = new ArrayList<String>();
-        PrintStream fakePrintStream = new PrintStream("aaa") {
-            @Override
-            public void println(String str) {
-                actual.add(str);
-            }
-        };
-
-        // Act
-        System.setOut(fakePrintStream);
         training2_3.practice1();
+        sos.clearOutput();
+        String actual = sos.getOutput();
+        String expected = "プログラミング言語の種類\n「Java」「C言語」「PHP」などがあります。\n";
 
-        // Assert
-        List<String> expected = Arrays.asList("プログラミング言語の種類", "「Java」「C言語」「PHP」などがあります。");
         assertEquals(actual, expected);
     }
 
     @Test
     public void practice2() throws Exception {
 
-        final List<String> actual = new ArrayList<String>();
-        PrintStream fakePrintStream = new PrintStream("aaa") {
-            @Override
-            public void println(String str) {
-                actual.add(str);
-            }
-        };
-
-        // Act
-        System.setOut(fakePrintStream);
         training2_3.practice2();
+        sos.clearOutput();
+        String actual = sos.getOutput();
+        String expected = "研修の講師はとても優しくて、\n真面目ないい人です。\n";
 
-        // Assert
-        List<String> expected = Arrays.asList("研修の講師はとても優しくて、", "真面目ないい人です。");
         assertEquals(actual, expected);
     }
 }
