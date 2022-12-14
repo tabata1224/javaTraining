60db0aaf8a0be58ba757366c9f6f5db3899e0227
Kitano <ryohei.kitano.works@gmail.com>
2022-12-14 08:25:16 +0000

    feat: チャプター3_2の問題を追加

---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter3/Training3_2.java
@@ -0,0 +1,170 @@
+package consallink.chapter3;
+
+public class Training3_2 {
+
+    public int practice1() {
+
+        // 問題1
+        // コマンドライン引数の説明として間違っているものを選んでください
+        // 1.コマンドライン引数はint型として利用できない
+        // 2.コマンドライン引数を利用しない場合は、main()メソッドの引数を「main(String[] args)」から「main()」に変更する
+        // 3.実行時「java Test a b c」と入力した場合、args.lengthを利用すると3が取得できる
+        // 4.main()メソッドを「main(int[] args)」と変更すると、コンパイルエラーは起こらないが、実行時にエラーとなる
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice2() {
+
+        // 問題2
+        // 「java Test 1 2 3」と実行した際にエラーとなる記述を選んでください
+        // 1.System.out.println(args[0]);
+        // 2.System.out.println(args[1]);
+        // 3.System.out.println(args[2]);
+        // 4.System.out.println(args[3]);
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice3() {
+
+        // 問題3
+        // 配列の説明として間違っているものを選んでください
+        // 1.配列はいつでも要素数を変更することができる
+        // 2.同一の配列内で違う型のデータを扱うことはできない
+        // 3.配列の実体を確保すると要素数分の初期値が自動的に代入される
+        // 4.配列のインデックスは0からスタートする
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice4() {
+
+        // 問題4
+        // 「String[] str = new String[3];」の要素数として正しいものを選んでください
+        // 1.0
+        // 2.1
+        // 3.2
+        // 4.3
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice5() {
+
+        // 問題5
+        // 「String[] str = {"a","b","c","d"};」の配列内から"c"の文字列を取得する際の記述を選んでください
+        // 1.str[0]
+        // 2.str[1]
+        // 3.str[2]
+        // 4.str[3]
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice6() {
+
+        // 問題6
+        // 「String[][] str = 〇;」〇の部分のプログラムとして正しい記述を選んでください
+        // 1.{{1,2},{3,4,5}}
+        // 2.{{"1","2"},{"3","4","5"}}
+        // 3.{{1,2},{"3","4","5"}}
+        // 4.{{1,2,3,4,5}}
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice7() {
+
+        // 問題7
+        // 「int[][] nums = {1,2,3,4,5};」を記述した際、nums.lengthで取得できる数値として正しいものを選んでください
+        // 1.5
+        // 2.4
+        // 3.3
+        // 4.2
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice8() {
+
+        // 問題8
+        // 「int[][] nums = {{1,2},{3,4,5}};」を記述した際、nums.lengthで取得できる数値として正しいものを選んでください
+        // 1.5
+        // 2.4
+        // 3.3
+        // 4.2
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice9() {
+
+        // 問題9
+        // 「int[][] nums = {{1,2,3,4,5},{6,7,8},{9,10}};」を記述した際、
+        // nums[2][1]で取得できる数値として正しいものを選んでください
+        // 1.4
+        // 2.5
+        // 3.7
+        // 4.10
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice10() {
+
+        // 問題10
+        // Integer.parseInt(〇)の部分に記述するプログラムとして正しい記述を選んでください
+        // 1."5"
+        // 2."4,4"
+        // 3.5
+        // 4.4.4
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int practice11() {
+
+        // 問題11
+        // 文字列を小数として変換するプログラムとして正しい記述を選んでください
+        // 1.Integer.parseInt("7.7")
+        // 2.Integer.parseDouble("7.7")
+        // 3.Double.parseInt("7.7")
+        // 4.Double.parseDouble("7.7")
+
+        // 変数answerに答えの選択肢の数値を代入しよう！
+        int answer = 0;
+
+        return answer;
+    }
+}
---
+++ JavaTraining/java-practice/src/test/java/consallink/chapter3/Training3_2_Test.java
@@ -0,0 +1,86 @@
+package consallink.chapter3;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Training3_2_Test {
+    private Training3_2 training3_2 = new Training3_2();
+
+    @Test
+    public void practice1() throws Exception {
+        int expected = 2;
+        int actual = training3_2.practice1();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice2() throws Exception {
+        int expected = 4;
+        int actual = training3_2.practice2();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice3() throws Exception {
+        int expected = 1;
+        int actual = training3_2.practice3();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice4() throws Exception {
+        int expected = 4;
+        int actual = training3_2.practice4();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice5() throws Exception {
+        int expected = 3;
+        int actual = training3_2.practice5();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice6() throws Exception {
+        int expected = 2;
+        int actual = training3_2.practice6();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice7() throws Exception {
+        int expected = 5;
+        int actual = training3_2.practice7();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice8() throws Exception {
+        int expected = 4;
+        int actual = training3_2.practice8();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice9() throws Exception {
+        int expected = 4;
+        int actual = training3_2.practice9();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice10() throws Exception {
+        int expected = 1;
+        int actual = training3_2.practice10();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void practice11() throws Exception {
+        int expected = 4;
+        int actual = training3_2.practice11();
+        assertThat(actual, is(expected));
+    }
+}
