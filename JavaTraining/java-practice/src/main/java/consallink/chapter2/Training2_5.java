c0d1e778d74c4279b84f1750b9457b413a7f31fc
Kitano <ryohei.kitano.works@gmail.com>
2022-12-15 09:09:15 +0000

    feat: エラーをコメントアウトするのではなく、正常な行を戻すように変更

--- JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_5.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter2/Training2_5.java
@@ -8,27 +8,27 @@ public class Training2_5 {
 
     public static void practice1() {
 
-        // 以下のプログラムのエラーが発生している部分をコメント行にしてください。
+        // 以下のプログラムのコメントアウトを、エラーが発生しない行のみ解除してください。
         // 修正後に結果を想像してから、実際に実行してみましょう。
         // プログラムの実行はmainメソッドの上に表示されている「Run」をクリックします。
 
         // 算術演算
-        System.out.println(5 + 3 * 2);
-        System.out.println(3 / 2 + 1);
-        System.out.println('あ' + 1);
-        System.out.println(5 / 2 * 2.5);
+        // System.out.println(5 + 3 * 2);
+        // System.out.println(3 / 2 + 1);
+        // System.out.println('あ' + 1);
+        // System.out.println(5 / 2 * 2.5);
 
         // 代入演算
-        int a = 5 / 2;
-        System.out.println(a);
-        double b = 5 / 2;
-        System.out.println(b);
-        float c = 3 / 2;
-        System.out.println(c);
-        byte d = 1;
-        byte e = d + d;
-        System.out.println(e);
-        float f = 3.14;
-        System.out.println(f);
+        // int a = 5 / 2;
+        // System.out.println(a);
+        // double b = 5 / 2;
+        // System.out.println(b);
+        // float c = 3 / 2;
+        // System.out.println(c);
+        // byte d = 1;
+        // byte e = d + d;
+        // System.out.println(e);
+        // float f = 3.14;
+        // System.out.println(f);
     }
 }
