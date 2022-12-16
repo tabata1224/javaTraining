33d0f9ec3806345804c8029d2757023d8a250d9e
don-yamada <ryohei.kitano.works@gmail.com>
2022-12-16 08:54:43 +0000

    feat: チャプター4_4_1の問題を追加

---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section1/Exam4_4_1.java
@@ -0,0 +1,24 @@
+package consallink.chapter4.Training4_4.section1;
+
+public class Exam4_4_1 {
+
+    public static void main(String[] args) {
+        execute();
+    }
+
+    public static void execute() {
+
+        /*
+         * 以下のプログラミングを記述してください。
+         * 1.Inspection4_4_1クラスからインスタンスを生成してください。
+         * 2.作成したインスタンスのフィールドに100を代入してください。
+         * 3.作成したインスタンスを利用してmethod()メソッドを呼び出してください。
+         * 
+         * 実行結果は以下のようにしてください。
+         * メソッドが実行されました
+         * numberには100が保存されています
+         */
+
+    }
+
+}
---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section1/Inspection4_4_1.java
@@ -0,0 +1,11 @@
+package consallink.chapter4.Training4_4.section1;
+
+public class Inspection4_4_1 {
+    int number;
+
+    public void method() {
+        System.out.println("メソッドが実行されました");
+        System.out.println("numberには" + number + "が保存されています");
+    }
+
+}
