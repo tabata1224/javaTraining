27e8da75ebe217bddce02803e013c05f9e39bc05
don-yamada <ryohei.kitano.works@gmail.com>
2022-12-16 08:55:25 +0000

    feat: チャプター4_4_3の問題を追加

---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section3/Exam4_4_3.java
@@ -0,0 +1,28 @@
+package consallink.chapter4.Training4_4.section3;
+
+public class Exam4_4_3 {
+
+    public static void main(String[] args) {
+        execute();
+    }
+
+    public static void execute() {
+
+        // double height = 1.71;
+        // double weight = 65.5;
+
+        /*
+         * 以下のプログラミングを記述してください
+         * 1.Inspection4_4_3クラスからインスタンスを生成してください
+         * 2.で作成したインスタンスの各種メソッドを利用してBMI計算を行ってください
+         * このクラス内のBMI計算では算術演算は利用しないこと
+         * 3.で計算した結果はdouble型のbmi変数に格納してください
+         * 4.各変数の値を表示してください
+         * 
+         * 参考（BMI計算式）：体重kg / (身長[m] * 身長[m])
+         * BMI計算式の例： 65.5 / (1.71 * 1.71)
+         */
+
+    }
+
+}
---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section3/Inspection4_4_3.java
@@ -0,0 +1,32 @@
+package consallink.chapter4.Training4_4.section3;
+
+/*
+ * 指示に従ってクラスを完成させてください。
+ * 
+ * メソッド
+ * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * 
+ * 【メソッド名】addtion
+ * 【戻り値】double
+ * 【引数】double a, double b
+ * 【処理内容】引数aとbを加算しreturnする
+ * 
+ * 【メソッド名】subtraction
+ * 【戻り値】double
+ * 【引数】double a, double b
+ * 【処理内容】引数aとbを減算しreturnする
+ * 
+ * 【メソッド名】multiplication
+ * 【戻り値】double
+ * 【引数】double a, double b
+ * 【処理内容】引数aとbを乗算しreturnする
+ * 
+ * 【メソッド名】division
+ * 【戻り値】double
+ * 【引数】double a, double b
+ * 【処理内容】引数aをbで除算しreturnする
+ */
+
+public class Inspection4_4_3 {
+
+}
