19e1ad2efd0b69e184c2a02b2e9ffed1741d4c82
don-yamada <ryohei.kitano.works@gmail.com>
2022-12-16 08:55:01 +0000

    feat: チャプター4_4_2の問題を追加

---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section2/Exam4_4_2.java
@@ -0,0 +1,28 @@
+package consallink.chapter4.Training4_4.section2;
+
+public class Exam4_4_2 {
+
+    public static void main(String[] args) {
+        execute();
+    }
+
+    public static void execute() {
+
+        /*
+         * 以下のプログラミングを記述してください。
+         * 1.Inspection4_4_2クラスからインスタンスを生成してください。
+         * 2.作成したインスタンスのフィールドに500を代入してください。
+         * 3.作成したインスタンスを利用してprintNumber()メソッドを実行してください。
+         * 但し、ループ文と組み合わせて5回表示を行うようにしてください
+         * 
+         * 実行結果は以下のようにしてください。
+         * 500
+         * 501
+         * 502
+         * 503
+         * 504
+         */
+
+    }
+
+}
---
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_4/section2/Inspection4_4_2.java
@@ -0,0 +1,20 @@
+package consallink.chapter4.Training4_4.section2;
+
+/*
+ * 指示に従ってクラスを完成させてください。
+ * 
+ *  * フィールド
+ * 【型】int 【変数名】number 【初期値】なし
+ * 
+ * メソッド
+ * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * 
+ * 【メソッド名】printNumber
+ * 【戻り値】void
+ * 【引数】なし
+ * 【処理内容】numberの値をインクリメント(後置)しながら表示する
+ */
+
+public class Inspection4_4_2 {
+
+}
