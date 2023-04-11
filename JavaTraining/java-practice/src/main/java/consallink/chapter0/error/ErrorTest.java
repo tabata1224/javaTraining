027b07e96d12bff0c5ada18a1967cf410b664d49
tabata1224 <masato.tabata@consallink.co.jp>
2023-04-11 00:22:52 +0000

    feat: エラーパターンを追加

--- JavaTraining/java-practice/src/main/java/consallink/chapter0/error/ErrorTest.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter0/error/ErrorTest.java
@@ -5,10 +5,18 @@ public class ErrorTest {
     public static void main(String[] args) {
 
         // java.lang.NullPointerExceptionを発生させよう！
-
+        //String str = null;
+        //System.out.println(str.length());
+        
         // java.lang.ArrayIndexOutOfBoundsExceptionを発生させよう！
+        // int[] num;
+        // num = new int[5];
+        // System.out.println(num[6]);
 
         // java.lang.NumberFormatExceptionを発生させよう！
+        //String str = "sss";
+        //int i = Integer.parseInt(str);
+        //System.out.println(i);
 
     }
 
