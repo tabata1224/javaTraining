5e9bd960d287f5e8c3a3572c271e0160d2055c2a
tabata1224 <masato.tabata@consallink.co.jp>
2023-04-18 00:20:04 +0000

    feat:Training4_10整形しました

--- JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_10/section1/Hero.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_10/section1/Hero.java
@@ -6,24 +6,15 @@ package consallink.chapter4.Training4_10.section1;
  * 
  * BattleCharacterクラスを継承する
  * 
- * メソッド
- * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * メソッド ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
  * 
- * 【メソッド名】コンストラクタ
- * 【引数】String name, int level, int hp, int mp, int attack, int m_attack, int defense, int m_defense
- * 【処理内容】引数で取得した値をスーパークラスのコンストラクタに引き渡す。typeは"勇者"を指定。
+ * 【メソッド名】コンストラクタ 【引数】String name, int level, int hp, int mp, int attack, int m_attack, int defense,
+ * int m_defense 【処理内容】引数で取得した値をスーパークラスのコンストラクタに引き渡す。typeは"勇者"を指定。
  * 
- * 【メソッド名】コンストラクタ
- * 【引数】なし
- * 【処理内容】
- * 値をスーパークラスのコンストラクタに引き渡す。値は以下を参考にすること。
- * name="ななし ひろ", level=1, hp=10, mp=8, attack=10, m_attack=8, defense=8, m_defense=7, type="勇者"
+ * 【メソッド名】コンストラクタ 【引数】なし 【処理内容】 値をスーパークラスのコンストラクタに引き渡す。値は以下を参考にすること。 name="ななし ひろ", level=1, hp=10,
+ * mp=8, attack=10, m_attack=8, defense=8, m_defense=7, type="勇者"
  * 
- * 【メソッド名】levelUp
- * 【戻り値】void
- * 【引数】なし
- * 【処理内容】
- * スーパークラスのフィールドの値を以下のように増加させるメソッド。
+ * 【メソッド名】levelUp 【戻り値】void 【引数】なし 【処理内容】 スーパークラスのフィールドの値を以下のように増加させるメソッド。
  * levelは+1、hpは+2、mpは+1、attackは+2、m_attackは+1、defenseは+1、m_defenseは+1
  */
 
@@ -35,7 +26,7 @@ public class Hero extends BattleCharacter {
     }
 
     Hero() {
-        super("ななし ひろ", 1, 10, 8, 10, 8, 8, 7,"勇者");
+        super("ななし ひろ", 1, 10, 8, 10, 8, 8, 7, "勇者");
 
     }
 
--- JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_10/section1/Warrior.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_10/section1/Warrior.java
@@ -5,34 +5,26 @@ package consallink.chapter4.Training4_10.section1;
  * 
  * BattleCharacterクラスを継承する
  * 
- * メソッド
- * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * メソッド ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
  * 
- * 【メソッド名】コンストラクタ
- * 【引数】String name, int level, int hp, int mp, int attack, int m_attack, int defense, int m_defense
- * 【処理内容】引数で取得した値をスーパークラスのコンストラクタに引き渡す。typeは"戦士"を指定。
+ * 【メソッド名】コンストラクタ 【引数】String name, int level, int hp, int mp, int attack, int m_attack, int defense,
+ * int m_defense 【処理内容】引数で取得した値をスーパークラスのコンストラクタに引き渡す。typeは"戦士"を指定。
  * 
- * 【メソッド名】コンストラクタ
- * 【引数】なし
- * 【処理内容】
- * 値をスーパークラスのコンストラクタに引き渡す。値は以下を参考にすること。
- * name="せん つよっし", level=1, hp=15, mp=0, attack=12, m_attack=0, defense=10, m_defense=5, type="戦士"
+ * 【メソッド名】コンストラクタ 【引数】なし 【処理内容】 値をスーパークラスのコンストラクタに引き渡す。値は以下を参考にすること。 name="せん つよっし", level=1, hp=15,
+ * mp=0, attack=12, m_attack=0, defense=10, m_defense=5, type="戦士"
  * 
- * 【メソッド名】levelUp
- * 【戻り値】void
- * 【引数】なし
- * 【処理内容】
- * スーパークラスのフィールドの値を以下のように増加させるメソッド。
+ * 【メソッド名】levelUp 【戻り値】void 【引数】なし 【処理内容】 スーパークラスのフィールドの値を以下のように増加させるメソッド。
  * levelは+1、hpは+3、mpは+0、attackは+3、m_attackは+0、defenseは+2、m_defenseは+1
  */
 
-public class Warrior extends BattleCharacter{
-    Warrior(String name, int level, int hp, int mp, int attack, int m_attack, int defense, int m_defense){
+public class Warrior extends BattleCharacter {
+    Warrior(String name, int level, int hp, int mp, int attack, int m_attack, int defense,
+            int m_defense) {
         super(name, level, hp, mp, attack, m_attack, defense, m_defense, "戦士");
     }
 
-    Warrior(){
-        super("せん つよし", 1, 15, 0, 12, 0, 10, 5,"戦士");
+    Warrior() {
+        super("せん つよし", 1, 15, 0, 12, 0, 10, 5, "戦士");
     }
 
     public void levelUp() {
