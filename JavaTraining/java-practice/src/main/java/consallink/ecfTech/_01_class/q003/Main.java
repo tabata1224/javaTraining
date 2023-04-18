e4b0e55517c53cbce63ec35c1d8aacd9e19b846a
tabata1224 <masato.tabata@consallink.co.jp>
2023-04-18 04:24:57 +0000

    feat:_01_class/q003解答しました

---
+++ JavaTraining/java-practice/src/main/java/consallink/ecfTech/_01_class/q003/Main.java
@@ -0,0 +1,16 @@
+package consallink.ecfTech._01_class.q003;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.name = "タロウ";
+        s1.score = 75;
+        s2.name = "ハナコ";
+        s2.score = 80;
+
+        s1.showScore();
+        s2.showScore();
+    }
+}
---
+++ JavaTraining/java-practice/src/main/java/consallink/ecfTech/_01_class/q003/Student.java
@@ -0,0 +1,11 @@
+package consallink.ecfTech._01_class.q003;
+
+public class Student {
+    String name;
+    int score;
+
+    public void showScore() {
+        System.out.println(name + "さんの得点は" + score + "点です。");
+    }
+}
+
