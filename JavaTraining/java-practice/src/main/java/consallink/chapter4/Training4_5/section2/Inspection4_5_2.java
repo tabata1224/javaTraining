511e122f7c5932a72c88c14f041f8396e39a21bc
tabata1224 <masato.tabata@consallink.co.jp>
2023-04-18 00:04:33 +0000

    feat:Training4-5整形しました

--- JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_5/section2/Inspection4_5_2.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_5/section2/Inspection4_5_2.java
@@ -3,33 +3,25 @@ package consallink.chapter4.Training4_5.section2;
 /*
  * 指示に従ってクラスを完成させてください。
  * 
- * メソッド
- * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * メソッド ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
  * 
- * 【メソッド名】param01
- * 【戻り値】void
- * 【引数】int x
- * 【処理内容】「x + "を受け取りました"」を表示する
+ * 【メソッド名】param01 【戻り値】void 【引数】int x 【処理内容】「x + "を受け取りました"」を表示する
  * 
- * 【メソッド名】param02
- * 【戻り値】void
- * 【引数】double x
- * 【処理内容】「x + "を受け取りました"」を表示する
+ * 【メソッド名】param02 【戻り値】void 【引数】double x 【処理内容】「x + "を受け取りました"」を表示する
  * 
- * 【メソッド名】param03
- * 【戻り値】void
- * 【引数】String x
- * 【処理内容】「x + "を受け取りました"」を表示する
+ * 【メソッド名】param03 【戻り値】void 【引数】String x 【処理内容】「x + "を受け取りました"」を表示する
  */
 
 public class Inspection4_5_2 {
-    public void param01(int x){
+    public void param01(int x) {
         System.out.println(x + "を受け取りました");
     }
-    public void param02(double x){
+
+    public void param02(double x) {
         System.out.println(x + "を受け取りました");
     }
-    public void param03(String x){
+
+    public void param03(String x) {
         System.out.println(x + "を受け取りました");
     }
 }
--- JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_5/section4/Inspection4_5_4.java
+++ JavaTraining/java-practice/src/main/java/consallink/chapter4/Training4_5/section4/Inspection4_5_4.java
@@ -3,38 +3,29 @@ package consallink.chapter4.Training4_5.section4;
 /*
  * 指示に従ってクラスを完成させてください。
  * 
- *  フィールド
- * 【型】int 【変数名】count 【初期値】なし
+ * フィールド 【型】int 【変数名】count 【初期値】なし
  * 
- * メソッド
- * ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
+ * メソッド ※処理内容内の引数名や{{ 計算結果 }}は適切に処理に置き換えてください。
  * 
- * 【メソッド名】countUp
- * 【戻り値】void
- * 【引数】なし
- * 【処理内容】countフィールドをインクリメントする
+ * 【メソッド名】countUp 【戻り値】void 【引数】なし 【処理内容】countフィールドをインクリメントする
  * 
- * 【メソッド名】countDowns
- * 【戻り値】void
- * 【引数】なし
- * 【処理内容】countフィールドをデクリメントする
+ * 【メソッド名】countDowns 【戻り値】void 【引数】なし 【処理内容】countフィールドをデクリメントする
  * 
- * 【メソッド名】print
- * 【戻り値】void
- * 【引数】なし
- * 【処理内容】「"現在のカウントは" + count + "です"」を表示する
+ * 【メソッド名】print 【戻り値】void 【引数】なし 【処理内容】「"現在のカウントは" + count + "です"」を表示する
  */
 
 public class Inspection4_5_4 {
     int count;
 
-    public void countUp(){
+    public void countUp() {
         count++;
     }
-    public void countDown(){
+
+    public void countDown() {
         count--;
     }
-    public void print(){
+
+    public void print() {
         System.out.println("現在のカウントは" + count + "です");
     }
 }
