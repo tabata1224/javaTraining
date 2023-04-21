7c9dfa44b5332141e7baf3ec5c3c4d993e3620bf
tabata1224 <masato.tabata@consallink.co.jp>
2023-04-21 01:10:17 +0000

    feat:ecfTech/_09_collection/q001を作成しました

---
+++ JavaTraining/java-practice/src/main/java/consallink/ecfTech/_09_collection/q001/Main.java
@@ -0,0 +1,19 @@
+package consallink.ecfTech._09_collection.q001;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("りんご");
+        fruits.add("みかん");
+        fruits.add("バナナ");
+        fruits.add("ぶどう");
+
+        for (int i = 0; i < fruits.size(); i++) {
+            System.out.println(fruits.get(i));
+        }
+    }
+}
+
+
